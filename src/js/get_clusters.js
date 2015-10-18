42f76f7c91e24cd52750cc16bd29384aefe89799
Ivan Strelkov <istrelkov@onlinetours.ru>
2015-10-18 23:27:41 +0300

    Step 8. Fix

--- src/js/get_clusters.js
+++ src/js/get_clusters.js
@@ -1,5 +1,13 @@
 'use strict';
 
 export default function getClusters(markers) {
-  return markers.slice(0, 2);
+  var sortedMarkers =
+    _(markers).sortBy(function(marker) {
+      return marker.latitude;
+    });
+
+  var minMarker = sortedMarkers.first();
+  var maxMarker = sortedMarkers.last();
+
+  return [minMarker, maxMarker];
 }

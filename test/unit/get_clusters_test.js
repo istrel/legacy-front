66d71df75df43fe6ce291aa57973cfc9a591b953
Ivan Strelkov <istrelkov@onlinetours.ru>
2015-10-18 23:53:07 +0300

    Step 9. Tests refactoring

--- test/unit/get_clusters_test.js
+++ test/unit/get_clusters_test.js
@@ -13,53 +13,36 @@ describe('clusterize', function() {
     expect( getClusters(markers) ).to.eql(markers);
   });
 
-  it('returns two clusters for two parts of closely related markers', function() {
-    var firstCluster = _.times(10, function() {
-      return {
-        latitude: 25 + Math.random(),
-        longitude: 36 + Math.random()
-      }
+  describe('with two clusters', function() {
+    beforeEach(function() {
+      this.cluster = function(n, lat, lng) {
+        return _.times(n, function() {
+          return {
+            latitude: lat + Math.random(),
+            longitude: lng + Math.random()
+          };
+        })
+      };
+      var firstCluster = this.cluster(10, 25, 36);
+
+      var secondCluster = this.cluster(10, -25, -36);
+
+      this.shuffledMarkers =
+        _(firstCluster)
+          .union(secondCluster)
+          .shuffle()
+          .value();
     });
 
-    var secondCluster = _.times(10, function() {
-      return {
-        latitude: -25 + Math.random(),
-        longitude: -36 + Math.random()
-      }
+    it('returns two clusters for two parts of closely related markers', function() {
+      expect( getClusters(this.shuffledMarkers) ).to.have.lengthOf(2);
     });
 
-    var shuffledMarkers =
-      _(firstCluster)
-        .union(secondCluster)
-        .shuffle()
-        .value();
+    it('returns proper value for first cluster', function() {
+      var result = getClusters(this.shuffledMarkers);
 
-    expect( getClusters(shuffledMarkers) ).to.have.lengthOf(2);
+      expect(result[0].latitude).to.be.within(-25, -24);
+    });
   });
 
-  it('returns proper value for first cluster', function() {
-    var firstCluster = _.times(10, function() {
-      return {
-        latitude: 25 + Math.random(),
-        longitude: 36 + Math.random()
-      }
-    });
-
-    var secondCluster = _.times(10, function() {
-      return {
-        latitude: -25 + Math.random(),
-        longitude: -36 + Math.random()
-      }
-    });
-
-    var shuffledMarkers =
-      _(firstCluster)
-        .union(secondCluster)
-        .shuffle()
-        .value();
-
-    var result = getClusters(shuffledMarkers);
-
-    expect(result[0].latitude).to.be.within(-25, -24);
-  });
 });

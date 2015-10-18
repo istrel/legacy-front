7f82e861dccbf31728a759f530e734ad5e20a5d6
Ivan Strelkov <istrelkov@onlinetours.ru>
2015-10-19 00:12:03 +0300

    Step 11. Fix added test and broke different one

--- src/js/get_clusters.js
+++ src/js/get_clusters.js
@@ -1,13 +1,23 @@
 'use strict';
 
 export default function getClusters(markers) {
-  var sortedMarkers =
-    _(markers).sortBy(function(marker) {
-      return marker.latitude;
+  var clusters = [];
+
+  function distance(lhs, rhs) {
+    return Math.abs(lhs.latitude - rhs.latitude) + Math.abs(lhs.longitude - rhs.longitude);
+  }
+
+  markers.forEach(function(marker) {
+    var properCluster = _.find(clusters, function(cluster) {
+      return distance(cluster, marker) < 2;
     });
 
-  var minMarker = sortedMarkers.first();
-  var maxMarker = sortedMarkers.last();
+    if (properCluster) {
+      null
+    } else {
+      clusters.push(marker);
+    }
+  });
 
-  return [minMarker, maxMarker];
+  return clusters;
 }

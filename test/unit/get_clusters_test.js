c2cacca602507d951caa2981df588c92ce7c0522
Ivan Strelkov <istrelkov@onlinetours.ru>
2015-10-19 00:27:01 +0300

    Step 16. Tests refactoring

--- test/unit/get_clusters_test.js
+++ test/unit/get_clusters_test.js
@@ -13,7 +13,7 @@ describe('clusterize', function() {
     expect( getClusters(markers) ).to.eql(markers);
   });
 
-  describe('with two clusters', function() {
+  describe('with clusters', function() {
     beforeEach(function() {
       this.cluster = function(n, lat, lng) {
         return _.times(n, function() {
@@ -23,64 +23,59 @@ describe('clusterize', function() {
           };
         })
       };
-      var firstCluster = this.cluster(10, 25, 36);
-
-      var secondCluster = this.cluster(10, -25, -36);
-
-      this.shuffledMarkers =
-        _(firstCluster)
-          .union(secondCluster)
-          .shuffle()
-          .value();
     });
 
-    it('returns two clusters for two parts of closely related markers', function() {
-      expect( getClusters(this.shuffledMarkers) ).to.have.lengthOf(2);
+    describe('with two clusters', function() {
+      beforeEach(function() {
+        var firstCluster = this.cluster(10, 25, 36);
+
+        var secondCluster = this.cluster(10, -25, -36);
+
+        this.shuffledMarkers =
+          _(firstCluster)
+            .union(secondCluster)
+            .shuffle()
+            .value();
+      });
+
+      it('returns two clusters for two parts of closely related markers', function() {
+        expect( getClusters(this.shuffledMarkers) ).to.have.lengthOf(2);
+      });
+
+      it('returns proper value for first cluster', function() {
+        var result = getClusters(this.shuffledMarkers);
+
+        expect(result[0].latitude).to.be.within(-25, -24);
+      });
     });
 
-    it('returns proper value for first cluster', function() {
-      var result = getClusters(this.shuffledMarkers);
+    describe('with three clusters', function() {
+      beforeEach(function() {
+        var firstCluster  = this.cluster(4,  25, -1);
+        var secondCluster = this.cluster(5,  25, 24);
+        var thirdCluster  = this.cluster(6,  25, 6);
 
-      expect(result[0].latitude).to.be.within(-25, -24);
-    });
-  });
+        this.shuffledMarkers =
+          _(firstCluster)
+            .union(secondCluster)
+            .union(thirdCluster)
+            .shuffle()
+            .value();
+      });
 
-  describe('with three clusters', function() {
-    beforeEach(function() {
-      this.cluster = function(n, lat, lng) {
-        return _.times(n, function() {
-          return {
-            latitude: lat + Math.random(),
-            longitude: lng + Math.random()
-          };
-        })
-      };
+      it('returns three clusters', function() {
+        expect( getClusters(this.shuffledMarkers) ).to.have.lengthOf(3);
+      });
 
-      var firstCluster  = this.cluster(4,  25, -1);
-      var secondCluster = this.cluster(5,  25, 24);
-      var thirdCluster  = this.cluster(6,  25, 6);
+      it('sets title for created clusters', function() {
+        var titles =
+          _( getClusters(this.shuffledMarkers) )
+            .pluck('title')
+            .sort()
+            .value();
 
-      this.shuffledMarkers =
-        _(firstCluster)
-          .union(secondCluster)
-          .union(thirdCluster)
-          .shuffle()
-          .value();
-    });
-
-    it('returns three clusters', function() {
-      expect( getClusters(this.shuffledMarkers) ).to.have.lengthOf(3);
-    });
-
-    it('sets title for created clusters', function() {
-      var titles =
-        _( getClusters(this.shuffledMarkers) )
-          .pluck('title')
-          .sort()
-          .value();
-
-      expect(titles).to.eql(['4', '5', '6']);
+        expect(titles).to.eql(['4', '5', '6']);
+      });
     });
   });
-
 });
